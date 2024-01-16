4d3a20508a7cc704fd1b0f0323b4be035f7ed1a5
Max Niederman <max@maxniederman.com>
2024-01-16 00:07:42 -0800

    improve downloader and add misc plots

--- analysis.jl
+++ analysis.jl
@@ -1,14 +1,64 @@
-using DataFrames, SQLite, Gadfly
+using DataFrames, DataFramesMeta, SQLite, Gadfly
 
-data = DataFrame(DBInterface.execute(SQLite.DB("rankings.db"), "SELECT * FROM rankings;"))
+n_top = 16
 
-plot(
-    data[data.rank.<=20, :],
-    x=:rank,
-    y=:points,
-    color=:year,
-    Geom.point,
-    Geom.line,
-    Scale.color_discrete(),
-    Guide.title("District Points by Rank and Year"),
-)
+df = DataFrame(DBInterface.execute(
+    SQLite.DB("rankings.db"),
+    """
+    SELECT * FROM rankings
+        WHERE year >= 2014
+        AND year != 2020
+        AND year != 2021;
+    """
+))
+
+# Team Shares of District Points by Rank and Year
+@chain df begin
+    @subset(_, :district .== "pnw")
+    groupby(_, [:year, :district])
+    @transform(_, :share = :points ./ sum(:points))
+    @subset(_, :rank .<= n_top)
+    plot(
+        _,
+        x=:rank,
+        y=:share,
+        color=:year,
+        linestyle=:district,
+        Geom.line,
+        Scale.color_discrete(),
+        Guide.title("Share of District Points by Rank"),
+    )
+end
+
+# Team Share of World-Qualifying District Points by Rank and Year
+@chain df begin
+    @subset(_, :district .== "pnw")
+    @subset(_, :rank .<= n_top)
+    groupby(_, [:year, :district])
+    @transform(_, :share = :points ./ sum(:points))
+    plot(
+        _,
+        x=:rank,
+        y=:share,
+        color=:year,
+        linestyle=:district,
+        Geom.line,
+        Scale.color_discrete(),
+        Guide.title("Team Share of World-Qualifying District Points by Rank"),
+    )
+end
+
+# Worlds-Qualifying Share of District Points by Year
+@chain df begin
+    groupby(_, [:year, :district])
+    @combine(_, :share = sum(:points .* (:rank .<= n_top)) / sum(:points))
+    plot(
+        _,
+        x=:year,
+        y=:share,
+        color=:district,
+        Geom.point,
+        Geom.line,
+        Guide.title("Worlds-Qualifying Share of District Points"),
+    )
+end
--- download.nu
+++ download.nu
@@ -1,7 +1,11 @@
 const db_path = ("rankings.db" | path expand)
 
-def main [district: string, year: int] {
-    sqlite3 rankings.db "CREATE TABLE IF NOT EXISTS rankings (year INTEGER, district TEXT, team INTEGER, rank INTEGER, points INTEGER, PRIMARY KEY (year, district, team));" ".exit"
+def "main district" [district: string, year: int] {
+    (sqlite3
+        $db_path
+        "PRAGMA journal_mode = WAL;"
+        "CREATE TABLE IF NOT EXISTS rankings (year INTEGER, district TEXT, team INTEGER, rank INTEGER, points INTEGER, PRIMARY KEY (year, district, rank));"
+        ".exit")
 
     http get -H [X-TBA-Auth-Key $env.TBA_AUTH_KEY] $"https://www.thebluealliance.com/api/v3/district/($year)($district)/rankings"
         | insert year $year
@@ -10,4 +14,15 @@ def main [district: string, year: int] {
         | select year district team_key rank point_total
         | rename year district team rank points
         | into sqlite -t rankings rankings.db
+}
+
+def "main year" [year: int] {
+    http get -H [X-TBA-Auth-Key $env.TBA_AUTH_KEY] $"https://www.thebluealliance.com/api/v3/districts/($year)"
+        | select abbreviation year
+        | rename district year
+        | each { |it| main district $it.district $it.year }
+}
+
+def "main" [] {
+    2014..2023 | each { |it| main year $it }
 }
